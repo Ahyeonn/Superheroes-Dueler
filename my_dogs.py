ebd4cf46f8f03496290ffe5a0d286b55dd2ce152
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-11-05 16:32:59 -0700

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,10 @@
+# my_dogs.py
+from dog import Dog
+my_dog = Dog("Rex", "SuperDog")
+my_dog.bark()
+
+annie_dog = Dog("Annie", "SmallDog")
+annie_dog.sit()
+
+lucas_dog = Dog("Fin", "MediumDog")
+lucas_dog.roll_over()
