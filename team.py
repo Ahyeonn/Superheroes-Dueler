6a24469ea8bc9ad27bea535bcab3fcfe2d78a2b1
Ahyeon Jeon <ahyeon@Ahyeons-MBP.attlocal.net>
2021-11-09 15:54:48 -0800

    implemented inheritance

---
+++ team.py
@@ -0,0 +1,9 @@
+class Team:
+    def __init__(self, name):
+        ''' Initialize your team with its team name and an empty list of heroes
+        '''
+        self.name = name
+        self.heroes = list()
+
+
+team1 = Team("hero team")
